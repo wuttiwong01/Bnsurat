4d7b68b92c8f7eb420c43e56bc61b36801ac0496
number27 <number27.th@gmail.com>
2016-12-10 10:48:32 +0700

    First Commit

---
+++ app/src/main/java/com/example/number27/bnsurat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.number27.bnsurat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
